0d7e2621fc30f4ffe8693086c59396b07d29a49b
Erskine Gamaliel L. Duenas <91664149+laharl143@users.noreply.github.com>
2022-08-10 18:00:14 +0800

    added initial files

---
+++ calculator.js
@@ -0,0 +1,30 @@
+
+// commands used: 
+//     $ npm install body-parser
+
+
+
+const express = require('express');
+const bodyParser = require("body-parser");    //incorporated body-parser
+const app = express();
+const port = 3000
+
+app.use(bodyParser.urlencoded({extended: true}));  //this is the code u need to write everytime u will use body-parser
+
+app.get('/', (req, res) => {              // "/" is the home page
+  res.sendFile(__dirname + "/index.html");            //res.sendFile is used if its a file ; __dirname is the current path of the file name
+})
+
+app.post("/", function(req, res) {
+
+var num1 = Number(req.body.num1); //req = request ; body is everything we got ; num1 is the name attribute of First number in index.html
+var num2 = Number(req.body.num2); //req = request ; body is everything we got ; num2 is the name attribute of Second number in index.html
+
+var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
